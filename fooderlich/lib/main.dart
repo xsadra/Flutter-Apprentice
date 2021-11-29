37a8bedf4d2550fff3b59ba30d11bd2e0e786b0c
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-29 22:49:42 +0100

    II-3.3-Setting a theme

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -1,66 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'fooderlich_theme.dart';
+
 void main() {
-  runApp(const MyApp());
+  // 1
+  runApp(const Fooderlich());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+
+    // TODO: Apply Home widget
+    // 3
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      theme: theme,
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Fooderlich',
+            style: theme.textTheme.headline6,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        body: Center(
+          child: Text('Let\'s get cooking 👩‍🍳',
+              style: theme.textTheme.headline1),
+        ),
       ),
     );
   }
