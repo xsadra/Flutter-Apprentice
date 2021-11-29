05a9c427a043557fd41bcbc5e4246b9a2855aa8e
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 00:17:36 +0100

    II-5.4-Creating a FutureBuilder

--- fooderlich/lib/screens/explore_screen.dart
+++ fooderlich/lib/screens/explore_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
 
 import '../api/mock_fooderlich_service.dart';
 
@@ -12,8 +13,30 @@ class ExploreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // 2
     // TODO 1: Add TodayRecipeListView FutureBuilder
-    return const Center(
-      child: Text('Explore Screen'),
+    // 1
+    return FutureBuilder(
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // TODO: Add Nested List Views
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          // TODO: Replace this with TodayRecipeListView
+          return Center(
+            child: Container(
+              child: const Text('Show TodayRecipeListView'),
+            ),
+          );
+        } else {
+          // 6
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
