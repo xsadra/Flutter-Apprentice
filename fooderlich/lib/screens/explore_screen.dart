0efdf46bf27122dfb23210bde543bd2ef4da7759
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 00:53:32 +0100

    II-5.8-Adding final touches to the Explore screen

--- fooderlich/lib/screens/explore_screen.dart
+++ fooderlich/lib/screens/explore_screen.dart
@@ -30,11 +30,7 @@ class ExploreScreen extends StatelessWidget {
               // 8
               const SizedBox(height: 16),
               // 9
-              // TODO: Replace this with FriendPostListView
-              Container(
-                height: 400,
-                color: Colors.green,
-              ),
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
             ],
           );
         } else {
