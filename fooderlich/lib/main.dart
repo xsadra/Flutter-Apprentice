dcde1dc00b6e34976fad23c67ae08c47778ed89c
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 12:48:15 +0100

    II-6.7-Managing the grocery items

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -22,7 +22,7 @@ class Fooderlich extends StatelessWidget {
       home: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => TabManager()),
-          // TODO 10: Add GroceryManager Provider
+          ChangeNotifierProvider(create: (context) => GroceryManager()),
         ],
         child: const Home(),
       ),
