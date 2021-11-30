5c594e6c1a6ccfa825a79a7f105fa6c8bb910bdd
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 22:00:18 +0100

    II-6.14-Creating GroceryListScreen

--- fooderlich/lib/screens/grocery_screen.dart
+++ fooderlich/lib/screens/grocery_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -39,8 +40,7 @@ class GroceryScreen extends StatelessWidget {
     return Consumer<GroceryManager>(
       builder: (context, manager, child) {
         if (manager.groceryItems.isNotEmpty) {
-          // TODO 25: Add GroceryListScreen
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           return const EmptyGroceryScreen();
         }
