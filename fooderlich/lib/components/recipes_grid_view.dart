cc1962c575aa62d79f247cf2efe807b6c833a669
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 11:40:39 +0100

    II-5.12-Challenge 2: Add a new GridView layout

--- fooderlich/lib/components/recipes_grid_view.dart
+++ fooderlich/lib/components/recipes_grid_view.dart
@@ -4,7 +4,6 @@ import '../components/components.dart';
 import '../models/models.dart';
 
 class RecipesGridView extends StatelessWidget {
-  // 1
   final List<SimpleRecipe> recipes;
 
   const RecipesGridView({
@@ -14,22 +13,17 @@ class RecipesGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 2
     return Padding(
       padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 16,
       ),
-      // 3
       child: GridView.builder(
-        // 4
         itemCount: recipes.length,
-        // 5
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 500.0),
         itemBuilder: (context, index) {
-          // 6
           final simpleRecipe = recipes[index];
           return RecipeThumbnail(recipe: simpleRecipe);
         },
