77be75e36fbb22298d78b0cf191c7b6a063bcb9e
Sadra <31988724+xsadra@users.noreply.github.com>
2021-11-30 11:34:22 +0100

    II-5.12-Challenge 1: Add a scroll listener

--- fooderlich/lib/screens/explore_screen.dart
+++ fooderlich/lib/screens/explore_screen.dart
@@ -1,43 +1,65 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:fooderlich/components/components.dart';
 import 'package:fooderlich/models/models.dart';
 
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
-  // 1
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
-  ExploreScreen({Key? key}) : super(key: key);
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // 1
     return FutureBuilder(
-      // 2
       future: mockService.getExploreData(),
-      // 3
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-        // 4
         if (snapshot.connectionState == ConnectionState.done) {
-          // 5
           return ListView(
-            // 6
+            controller: _controller,
             scrollDirection: Axis.vertical,
             children: [
-              // 7
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
-              // 8
               const SizedBox(height: 16),
-              // 9
               FriendPostListView(friendPosts: snapshot.data?.friendPosts ?? []),
             ],
           );
         } else {
-          // 10
           return const Center(child: CircularProgressIndicator());
         }
       },
     );
+  }
+
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      log('reached the bottom', name: 'ExploreScreen');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      log('reached the top!', name: 'ExploreScreen');
+    }
   }
 }
