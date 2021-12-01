fa534ed948cc61e9e037ee3fecf46e6a3b826cac
Sadra <31988724+xsadra@users.noreply.github.com>
2021-12-01 16:09:08 +0100

    III-7.11-Creating the router

---
+++ fooderlich/lib/navigation/app_router.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+
+  final GroceryManager groceryManager;
+
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      pages: [
+        // TODO: Add SplashScreen
+        // TODO: Add LoginScreen
+        // TODO: Add OnboardingScreen
+        // TODO: Add Home
+        // TODO: Create new item
+        // TODO: Select GroceryItemScreen
+        // TODO: Add Profile Screen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+
+    // TODO: Handle Onboarding and splash
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+    // 6
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
