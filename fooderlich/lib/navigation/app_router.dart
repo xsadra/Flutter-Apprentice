079e0a31d446383bbb86d12b97b10c5f11e82ebe
Sadra <31988724+xsadra@users.noreply.github.com>
2021-12-01 17:07:12 +0100

    III-7.19-Showing the Grocery Item screen

--- fooderlich/lib/navigation/app_router.dart
+++ fooderlich/lib/navigation/app_router.dart
@@ -48,8 +48,24 @@ class AppRouter extends RouterDelegate
         if (appStateManager.isOnboardingComplete)
           Home.page(appStateManager.getSelectedTab),
 
-        // TODO: Create new item
-        // TODO: Select GroceryItemScreen
+        if (groceryManager.isCreatingNewItem)
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              groceryManager.addItem(item);
+            },
+            onUpdate: (item, index) {},
+          ),
+
+        if (groceryManager.selectedIndex != -1)
+          GroceryItemScreen.page(
+            item: groceryManager.selectedGroceryItem,
+            index: groceryManager.selectedIndex,
+            onUpdate: (item, index) {
+              groceryManager.updateItem(item, index);
+            },
+            onCreate: (_) {},
+          ),
+
         // TODO: Add Profile Screen
         // TODO: Add WebView Screen
       ],
@@ -65,7 +81,10 @@ class AppRouter extends RouterDelegate
       appStateManager.logout();
     }
 
-    // TODO: Handle state when user closes grocery item screen
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+
     // TODO: Handle state when user closes profile screen
     // TODO: Handle state when user closes WebView screen
     // 6
