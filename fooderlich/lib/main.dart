2600051dea83523cd3798f08003e30172f85eb90
Sadra <31988724+xsadra@users.noreply.github.com>
2021-12-01 16:09:40 +0100

    III-7.12-Using your app router

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -3,8 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
-// TODO: Import app_router
+import 'navigation/app_router.dart';
 
 void main() {
   runApp(
@@ -24,9 +23,17 @@ class _FooderlichState extends State<Fooderlich> {
   final _profileManager = ProfileManager();
   final _appStateManager = AppStateManager();
 
-  // TODO: Define AppRouter
+  late AppRouter _appRouter;
 
-  // TODO: Initialize app router
+  @override
+  void initState() {
+    super.initState();
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,8 +62,10 @@ class _FooderlichState extends State<Fooderlich> {
             debugShowCheckedModeBanner: false,
             theme: theme,
             title: 'Fooderlich',
-            // TODO: Replace with Router widget
-            home: const SplashScreen(),
+            home: Router(
+              routerDelegate: _appRouter,
+              // TODO: Add backButtonDispatcher
+            ),
           );
         },
       ),
