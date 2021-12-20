abd91708ee6c3403503922f59b05aa8454f09a02
Sadra <31988724+xsadra@users.noreply.github.com>
2021-12-20 20:29:15 +0100

    IV-13.9-Convert data into models to display

--- recipes-iv/lib/network/recipe_model.dart
+++ recipes-iv/lib/network/recipe_model.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:recipes/data/models/models.dart';
 
 part 'recipe_model.g.dart';
 
@@ -97,4 +98,11 @@ class APIIngredients {
   Map<String, dynamic> toJson() => _$APIIngredientsToJson(this);
 }
 
-// TODO: Add convertIngredients() here
+List<Ingredient> convertIngredients(List<APIIngredients> apiIngredients) {
+  final ingredients = <Ingredient>[];
+  apiIngredients.forEach((ingredient) {
+    ingredients
+        .add(Ingredient(name: ingredient.name, weight: ingredient.weight));
+  });
+  return ingredients;
+}
